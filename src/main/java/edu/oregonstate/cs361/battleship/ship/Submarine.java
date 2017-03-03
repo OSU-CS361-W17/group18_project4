db6eed8ca3630cd674d6809f6f30319e846e5026
mengsk09876 <175974705@qq.com>
2017-03-03 15:54:06 -0800

    Update Submarine.java

--- src/main/java/edu/oregonstate/cs361/battleship/ship/Submarine.java
+++ src/main/java/edu/oregonstate/cs361/battleship/ship/Submarine.java
@@ -7,9 +7,4 @@ public class Submarine extends MilitaryShip {
 	public Submarine(Coordinate s, Coordinate e) {
 		super("submarine", 2, s, e);
 	}
-
-	@Override
-	public boolean scan(Coordinate test) {
-		return false;
-	}
 }
